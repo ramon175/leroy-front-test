b4664eb067c8a382948aa487e755a04350c4c0e0
Ramon Castan <ramon.castan175@gmail.com>
2018-02-23 10:27:43 -0300

    major changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import AppRouter from './routers/AppRouter';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const store = configureStore();
 
-export default App;
+const jsx = (
+    <Provider store={store} >
+        <AppRouter />
+    </Provider>
+);
+
+console.log(store.getState())
+
+ReactDOM.render(jsx, document.getElementById('app'));
